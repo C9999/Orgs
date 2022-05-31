8cfbb8d037ff661d7937bc84f299aceb6b0c9030
C9999 <car_soad@hotmail.com>
2022-05-31 18:19:39 -0300

    adicionando Views na Activity

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,11 +2,14 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Hello World Orgs", Toast.LENGTH_LONG).show()
+        val view = TextView(this)
+        view.setText("Cesta de frutas")
+        setContentView(view)
     }
 }
