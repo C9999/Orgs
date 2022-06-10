458e74f48301035fe1da64004874d4ed4dccde79
C9999 <car_soad@hotmail.com>
2022-06-10 15:59:42 -0300

    configurando o listener de clique

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutosActivity.kt
@@ -1,8 +1,23 @@
 package br.com.alura.orgs.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.R
 
 class FormularioProdutosActivity : AppCompatActivity(R.layout.activity_formulario_produtos) {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
 
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProdutox", "onCreate: $nome")
+        }
+    }
 }
